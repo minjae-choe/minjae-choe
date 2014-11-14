ac916655eb817eb408e366e2595af2f76b62b3ce
minjae-choe <q77q79@naver.com>
2014-11-14 11:11:37 +0900

    1114_2

--- 1114.py
+++ 1114.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 
+import random
 
 class myGame:
 	def _init_(self):
@@ -13,6 +15,19 @@ class updownGame(myGame):
 	def _init_(self):
 		pass
 
+	def start(self):
+		rn = random.randint(1,9)
+
+		while True:
+			an = input("Input:")
+
+			if an == rn:
+				print "Right"
+			elif an > rn:
+				print "Down"
+			else:
+				print "Up"
+
 class baseballGame(myGame):
 	def _init_(self):
 		pass
@@ -21,3 +36,6 @@ class baseballGame3(baseballGame):
 	def _init_(self):
 		pass
 
+if __name__ == '__main__':
+	myclass = updownGame()
+	myclass.start()
