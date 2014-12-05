b1936f136ddd1bcbbe2b9d368f53669b4138f1f5
minjae-choe <q77q79@naver.com>
2014-12-05 10:53:26 +0900

    1205scikit

---
+++ 1205_scikit.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 05 10:49:30 2014
+
+@author: user
+"""
+
+from sklearn import datasets
+iris = datasets.load_iris()
+digits = datasets.load_digits()
+
+print iris
+
+print digits
