9419c98cc7a59f51a55f100d5468bd97feb36ccf
yongkwice <yongkwice@gmail.com>
2014-12-05 09:25:15 +0900

    1205출석

---
+++ 1205.py
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print obj.values
+
+print obj.index
+
+obj2 = Series(lst,
+                  index=[]
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+print obj2['a']
+
+obj2['d'] = 6
+ 
+print obj2
+ 
+print obj2[['a','c','d']]
+
+obj222 = (obj2[obj2>0])
+
+print obj222
+
+
+
+
+
+
